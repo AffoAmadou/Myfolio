f882878b7a195bce20a7ce4221b3e520c9d70c98
Amadou Affo <62760390+AffoAmadou@users.noreply.github.com>
2024-09-30 00:56:16 +0200

    go

---
+++ public/main.768ccbdf473eb0ea1c9b.hot-update.js
@@ -0,0 +1,2 @@
+"use strict";self.webpackHotUpdatestarter("main",{},(function(t){t.h=()=>"69f4da01169ac3cf5461"}));
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi43NjhjY2JkZjQ3M2ViMGVhMWM5Yi5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiJpRUFBQUEsRUFBb0JDLEVBQUksSUFBTSxzQiIsInNvdXJjZXMiOlsid2VicGFjazovL3N0YXJ0ZXIvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbIl9fd2VicGFja19yZXF1aXJlX18uaCA9ICgpID0+IChcIjY5ZjRkYTAxMTY5YWMzY2Y1NDYxXCIpIl0sIm5hbWVzIjpbIl9fd2VicGFja19yZXF1aXJlX18iLCJoIl0sInNvdXJjZVJvb3QiOiIifQ==
